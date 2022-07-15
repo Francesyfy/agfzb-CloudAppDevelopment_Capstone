9cb4776f8ea77aebe2909995399d870a24643899
Frances Yuan <francesy@francess-mbp.lan>
2022-07-15 00:03:56 -0400

    build models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils.timezone import now
 
-
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
@@ -9,6 +8,12 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=50)
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,9 +24,29 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=50)
+    dealer_id = models.IntegerField()
+
+    SEDAN = "Sedan"
+    SUV = "SUV"
+    WAGON = "WAGON"
+    TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'WAGON')
+        ]
+    type = models.CharField(max_length=50, choices=TYPE_CHOICES)
+
+    year = models.DateField()
+
+    def __str__(self):
+        return self.name + ', ' + self.type
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+# class CarDealer():
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
