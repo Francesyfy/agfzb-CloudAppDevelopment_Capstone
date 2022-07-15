ef53bec077319d0577c015eed33f7dbf5c486894
Frances Yuan <francesy@francess-mbp.lan>
2022-07-15 02:17:08 -0400

    Django proxy of cloud functions

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -13,7 +13,7 @@ class CarMake(models.Model):
     description = models.CharField(max_length=1000)
 
     def __str__(self):
-        return self.name
+        return "Car make name: " + self.name
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -42,11 +42,53 @@ class CarModel(models.Model):
     year = models.DateField()
 
     def __str__(self):
-        return self.name + ', ' + self.type
+        return "Car model name: " + self.name
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
-# class CarDealer():
+class CarDealer:
+
+    def __init__(self, address, city, full_name, id, lat, long, short_name, state, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.state = state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview:
+
+    def __init__(self, id, name, dealership, review, purchase, purchase_date, car_make, car_model, car_year, sentiment):
+        self.id = id
+        self.name = name
+        self.dealership = dealership
+        self.id = id
+        self.review = review
+        self.purchase = purchase
+        self.purchase_date = purchase_date
+        self.car_make = car_make
+        self.car_model = car_model
+        self.car_year = car_year
+        self.sentiment = sentiment
+
+    def __str__(self):
+        return "Review: " + self.review
+
